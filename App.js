127d6a4bf07729d02f5338d7e43d3bd5177499d6
juliavalentim <julia.s.valentim12@gmail.com>
2023-01-16 16:51:53 -0300

    aula 86

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from "./navigation/DrawerNavigator";
+import LoginScreen from './screen/loginScreen';
+import RegisterScreen from './screen/register';
+import { createStackNavigator } from '@react-navigation/stack';
+import firebase from 'firebase';
+import { firebaseConfig } from './config';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+} else {
+  firebase.app()
+}
+
+const Stack = createStackNavigator()
+
+const StackNav = () => {
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name='login' component={LoginScreen}/> 
+      <Stack.Screen name='register' component={RegisterScreen}/>
+      <Stack.Screen name='dashboard' component={DrawerNavigator}/>
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <DrawerNavigator />
+      <StackNav/>
     </NavigationContainer>
   );
 }
---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDhU1C4FAkQ8ytqnyaiNNVKzCUq4mTzsbQ",
+  authDomain: "story-telling-cdc84.firebaseapp.com",
+  databaseURL: "https://story-telling-cdc84-default-rtdb.firebaseio.com",
+  projectId: "story-telling-cdc84",
+  storageBucket: "story-telling-cdc84.appspot.com",
+  messagingSenderId: "807725445629",
+  appId: "1:807725445629:web:eb4dd60d97a9144440a994"
+};
+
